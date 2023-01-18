578d14107592acc4f4107a429ac9eaf6b187457d
Shahar Bar <shahar@pocketpie.games>
2023-01-18 20:49:09 +0200

    first commit

---
+++ Assets/_HOG/Scripts/Core/HOGGameLoader.cs
@@ -0,0 +1,14 @@
+public class HOGGameLoader : HOGMonoBehaviour
+{
+    private void Start()
+    {
+        //TODO: User different Design Pattern
+        Invoke("DelayStart", 0.1f);
+    }
+
+    private void DelayStart()
+    {
+        new HOGManager();
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/_HOG/Scripts/Core/HOGTester.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class HOGTester : HOGMonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(Manager.GetNumber());
+        }
+    }
+}
